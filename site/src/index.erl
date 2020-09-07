ed9129a45cfc44bc4665f80bd18e4a78f8fdf9c2
Jesse Gumm <gumm@sigma-star.com>
2020-09-07 12:21:26 -0500

    Section 9.6

--- site/src/index.erl
+++ site/src/index.erl
@@ -3,37 +3,19 @@
 -compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
 
-main() -> #template { file="./site/templates/bare.html" }.
+main() -> #template { file="./site/templates/n_apps.html" }.
 
-title() -> "Welcome to Nitrogen".
+title() ->
+    "Welcome to nnote".
 
-body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
+top() ->
+    #h1 {text="Build it with Nitrogen"}.
 
-inner_body() -> 
-    [
-        #h1 { text="Welcome to Nitrogen" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-		#p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        ",
-		#p{},
-		"
-		<b>Want to see the ",#link{text="Sample Nitrogen jQuery Mobile Page",url="/mobile"},"?</b>
-		"
-    ].
-	
-event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
-    }).
+main_menu() ->
+    #h2 {text="Main Menu"}.
+
+sidebar() ->
+    #panel {text="Sidebar"}.
+
+content() ->
+    #panel {text="Content"}.
