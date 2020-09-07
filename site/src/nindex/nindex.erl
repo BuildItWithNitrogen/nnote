37ce0014e47287d1e2f13484f4edd8f7f2a482fa
Jesse Gumm <gumm@sigma-star.com>
2020-09-07 16:12:53 -0500

    9.12

---
+++ site/src/nindex/nindex.erl
@@ -0,0 +1,92 @@
+%% -*- mode: nitrogen -*-
+-module (nindex).
+-compile(export_all).
+-include_lib("nitrogen_core/include/wf.hrl").
+
+%% ***************************************************
+%% Macros
+%% ***************************************************
+-define(MMSELECTED, "nindex").
+-define(TITLE, "Welcome to nindex!").
+-define(TOP, "nindex").
+url_vars() -> [id, note_type, task].
+
+%% ***************************************************
+%% Template and Title
+%% ***************************************************
+
+main() ->
+    n_common:template().
+
+title() ->
+    ?TITLE.
+
+%% ***************************************************
+%% Panel definitions
+%% ***************************************************
+
+top() ->
+    #h1 {text=?TOP}.
+
+main_menu() ->
+    n_menus:show_main_menu(?MMSELECTED).
+
+content(#{}) ->
+    greeting().
+
+greeting() ->
+    [#h2{text=["Welcome to ", n_utils:get_nickname(),
+               "' ", "Nitrogen Applications!"]},
+     #p{body="Our motto: <em>\"Build it Fast with Nitrogen\"</em>"}
+    ].
+
+%% ***************************************************
+%% Tips
+%% ***************************************************
+tips() ->
+    [ #h2{text="Tips & Info"},
+      #p{body="The applications in this framework
+         were developed by Jesse Gumm and
+         Lloyd R. Prentice for their book
+         <em>Build it with Nitrogen</em>. These
+         applications are available for use and
+         modification under the MIT License."}
+    ].
+
+
+%% ***************************************************
+%% Sidebar menus
+%% ***************************************************
+side_menu("WEB SITE") ->
+    [{"nitrogen", {goto,
+                   "http://nitrogenproject.com/"}},
+     {"erlang", {goto,
+                 "http://erlang.org/doc/apps/stdlib/"}},
+     {"hacker news", {goto,
+                      "https://news.ycombinator.com/"}}
+    ].
+
+%% ***************************************************
+%% Sidebar events
+%% ***************************************************
+event({goto, Link}) ->
+    wf:redirect(Link).
+
+%% ***************************************************
+%% Sidebar executives
+%% ***************************************************
+sidebar(#{}) ->
+    [ #h3 {text="SELECT"},
+      show_side_menu("WEB SITE", unselected)
+    ].
+
+%% ***************************************************
+%% Sidebar functions
+%% ***************************************************
+show_side_menu(Menu, Selected) ->
+    [ #h4 {class=select, text=Menu},
+      [n_menus:show_menu_item(MenuItem, Selected) ||
+       MenuItem <- side_menu(Menu)]
+    ].
+
+
---
+++ site/src/nnote/nnote.erl
@@ -0,0 +1,89 @@
+%% -*- mode: nitrogen -*-
+-module (nnote).
+-compile(export_all).
+-include_lib("nitrogen_core/include/wf.hrl").
+
+%% ***************************************************
+%% Macros
+%% ***************************************************
+-define(MMSELECTED, "nnote").
+-define(TITLE, "Welcome to nnote!").
+-define(TOP, "nnote").
+url_vars() -> [id, note_type, task].
+
+%% ***************************************************
+%% Template and Title
+%% ***************************************************
+
+main() ->
+    n_common:template().
+
+title() ->
+    ?TITLE.
+
+%% ***************************************************
+%% Panel definitions
+%% ***************************************************
+
+top() ->
+    #h1 {text=?TOP}.
+
+main_menu() ->
+    n_menus:show_main_menu(?MMSELECTED).
+
+content(#{}) ->
+    content_headline().
+
+content_headline() ->
+    [#h2 {class=content, text="My Notes"}].
+
+%% ***************************************************
+%% Tips
+%% ***************************************************
+tips() ->
+    [ #h2{text="Tips & Info"},
+      #p{body="The applications in this framework
+         were developed by Jesse Gumm and
+         Lloyd R. Prentice for their book
+         <em>Build it with Nitrogen</em>. These
+         applications are available for use and
+         modification under the MIT License."}
+    ].
+
+
+%% ***************************************************
+%% Sidebar menus
+%% ***************************************************
+side_menu("WEB SITE") ->
+    [{"nitrogen", {goto,
+                   "http://nitrogenproject.com/"}},
+     {"erlang", {goto,
+                 "http://erlang.org/doc/apps/stdlib/"}},
+     {"hacker news", {goto,
+                      "https://news.ycombinator.com/"}}
+    ].
+
+%% ***************************************************
+%% Sidebar events
+%% ***************************************************
+event({goto, Link}) ->
+    wf:redirect(Link).
+
+%% ***************************************************
+%% Sidebar executives
+%% ***************************************************
+sidebar(#{}) ->
+    [ #h3 {text="SELECT"},
+      show_side_menu("WEB SITE", unselected)
+    ].
+
+%% ***************************************************
+%% Sidebar functions
+%% ***************************************************
+show_side_menu(Menu, Selected) ->
+    [ #h4 {class=select, text=Menu},
+      [n_menus:show_menu_item(MenuItem, Selected) ||
+       MenuItem <- side_menu(Menu)]
+    ].
+
+
