f620946165f4d7af2de670f088fc1140db8f5ced
Jesse Gumm <gumm@sigma-star.com>
2020-09-12 12:55:28 -0500

    10.2

--- site/src/n_libs/n_utils.erl
+++ site/src/n_libs/n_utils.erl
@@ -2,3 +2,17 @@
 -compile(export_all).
 
 get_nickname() -> "Marsha".
+
+create_id() ->
+    Rand = rand:uniform(1000000000),
+    Seconds = qdate:unixtime(),
+    GigaSeconds = Seconds * 1000000000,
+    ID = GigaSeconds + Rand,
+    integer_to_list(ID, 16).
+
+id_created(ID) ->
+    IntID = list_to_integer(ID, 16),
+    Seconds = IntID div 1000000000,
+    qdate:to_date(Seconds).
+
+
